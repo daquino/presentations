81fdaf135c9e9af6b516d0450b45674a23223d8f
Emily Stolfo <emily@10gen.com>
2013-09-04 13:50:38 -0400

    updates to example code

--- 20130920-gogaruco/threads.rb
+++ 20130920-gogaruco/threads.rb
@@ -4,15 +4,97 @@ members = Set.new
 threads = []
 
 50.times do |n|
-  if n % 2 == 0
-    threads << Thread.new do
+  threads << Thread.new do
+    if n % 2 == 0
       members << n
-    end
-  else
-    threads << Thread.new do
-      members.first * 2
+    else
+      members.first.nil?
     end
   end
 end
 
 threads.each(&:join)
+
+
+class MongoClient
+
+  def initialize
+    ...
+    @nodes = Set.new
+  end
+
+  def refresh
+    disconnect_old_nodes
+    connect_to_nodes
+    initialize_pools(@nodes)
+  end
+
+...
+
+  def connect_to_nodes
+    seed = valid_node
+    seed.node_list.each do |host|
+      node = Node.new(host)
+      @nodes << node if node.connect
+    end
+  end
+
+end
+
+
+class MongoClient
+
+  def initialize
+    ...
+    @nodes = Set.new
+    @connect_mutex = Mutex.new
+  end
+
+  def refresh
+    @connect_mutex.synchronize do
+      disconnect_old_nodes
+      connect_to_nodes
+      initialize_pools(@nodes)
+    end
+  end
+
+...
+
+  def connect_to_nodes
+    seed = valid_node
+    seed.node_list.each do |host|
+      node = Node.new(host)
+      @nodes << node if node.connect
+    end
+  end
+
+end
+
+class Pool
+...
+
+  def checkout
+    loop do
+      tid = Thread.current.object_id
+      @lock.synchronize do
+        ... # Thread gets its prior socket
+        if @sockets.size < @max_pool_size
+          socket = create_connection
+          return socket
+        elsif @checked_out.size < @sockets.size
+          socket = available[rand(available.size)]
+          return socket
+        end
+        @queue.wait(@lock)
+      end
+    end
+  end
+
+  def checkin(socket)
+    @lock.synchronize do
+      @checked_out.delete(socket)
+      @queue.signal
+    end
+  end
+
+end
